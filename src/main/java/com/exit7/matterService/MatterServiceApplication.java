488fefd4c4febcbc0d8be47732597416a9620bcb
Gimdohun <99ehgns@naver.com>
2023-04-01 03:51:26 +0900

    setting matterService

---
+++ src/main/java/com/exit7/matterService/MatterServiceApplication.java
@@ -0,0 +1,13 @@
+package com.exit7.matterService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatterServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatterServiceApplication.class, args);
+	}
+
+}
